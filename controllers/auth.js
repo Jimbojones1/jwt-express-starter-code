a08ce6ec0ba6b7c0bc9f5bdbadf432bf40ab0830
jimbojones1 <jimmyhaff3@gmail.com>
2025-02-14 15:43:06 -0500

    finished login

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,50 @@ const UserModel = require('../models/user')
 
 const saltRounds = 10;
 
+
+// Expectations
+// is the client is sending an object with a username and password key!
+router.post('/sign-in', async function(req, res){
+    try {
+
+        // search for the user by the username from the request
+        const user = await UserModel.findOne({username: req.body.username})
+
+        // if there is no user
+        if(!user){
+            console.log('THERE WAS NO USER')
+            return res.status(401).json({err: "Invalid credentials"})
+        }
+
+        // check the passwords
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.hashedPassword)
+        // wrong password
+        if(!isPasswordCorrect){
+            console.log("PASSWORD INCORRECT")
+            return res.status(401).json({err: 'Invalid Credentails'})
+        }
+
+        // create the token
+        const token = jwt.sign({payload: user}, process.env.JWT_SECRET)
+
+        res.status(200).json({token})
+
+
+    } catch(err){
+        console.log(err)
+        res.status(500).json({err: err.message})
+    }
+})
+
+
+
+
+
+
+
+
+
+
 // EXPECTATIONS -
 // username and password key on the object being submitted
 // to this route
